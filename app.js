a934edb4f7a51eb35a2b2f574f7dace2a1a1d288
Marco A Morales <marcoamorales@gmail.com>
2014-07-24 14:06:40 -0500

    Fix env variable being a string

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var PORT = process.env.PORT || 8000;
+var PORT = Number(process.env.PORT || 8000);
 
 http.createServer(function (req, res) {
   console.log('%d request received', process.pid);
